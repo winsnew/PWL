bd83d86ae1ad8a8926bbaed7a0ec8a701423268f
winsnew <92558826+winsnew@users.noreply.github.com>
2022-04-19 20:49:52 +0700

    Add files via upload

---
+++ utspwl1/uaspwl4.sql
@@ -0,0 +1,265 @@
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+CREATE TABLE 'barang' (
+  'kode_barang' int(11) NOT NULL,
+  `nama` varchar(50) NOT NULL,
+  `harga` int(11) NOT NULL,
+  `gambar` varchar(255) NOT NULL,
+  `jumlah_stok` varchar(255) NOT NULL,
+  
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `barang`
+--
+
+INSERT INTO `barang` (`kode_barang`, `nama`, `harga`, `gambar`, `jumlah_stok`) VALUES
+('001', 'Pensil',  '1500',  'PCS', '101',  NULL),
+('002',  'Sabun',  '1800',  'PCS', '38',),
+('003',  'Pulpen',  '1500',  'PCS', '68',  NULL),
+('004',  'Sprite',  '10000',  'PCS', '9', ),
+('005',  'ASUS A516JP I5',   '11500000', 'PCS', '60' NULL),
+('006',  'Mouse',  '50000',  'PCS', '100',  NULL),
+('007',  'ASUS ROG GX501VI-GZ001T - Black',  '50000000',  'PCS', '5',NULL),
+('008', 'HDD Acer 1000GB',  '300000',  'PCS', '12',  NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `kategori`
+--
+
+CREATE TABLE `kategori` (
+  `id_kategori` int(11) NOT NULL,
+  `nama_kategori` varchar(255) NOT NULL,
+  `tgl_input` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `kategori`
+--
+
+INSERT INTO `kategori` (`id_kategori`, `nama_kategori`, `tgl_input`) VALUES
+(8, 'Laptop', '16 July 2021, 17:40'),
+(11, 'Memori', '16 July 2021, 17:52');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `login`
+--
+
+CREATE TABLE `login` (
+  `id_login` int(11) NOT NULL,
+  `user` varchar(255) NOT NULL,
+  `pass` char(32) NOT NULL,
+  `id_member` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `login`
+--
+
+INSERT INTO `login` (`id_login`, `user`, `pass`, `id_member`) VALUES
+(1, 'admin', 'admin123', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `member`
+--
+
+CREATE TABLE `member` (
+  `id_member` int(11) NOT NULL,
+  `nm_member` varchar(255) NOT NULL,
+  `alamat_member` text NOT NULL,
+  `telepon` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `gambar` text NOT NULL,
+  `NIK` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+
+CREATE TABLE `nota` (
+  `id_nota` int(11) NOT NULL,
+  `id_barang` varchar(255) NOT NULL,
+  `id_member` int(11) NOT NULL,
+  `jumlah` varchar(255) NOT NULL,
+  `total` varchar(255) NOT NULL,
+  `tanggal_input` varchar(255) NOT NULL,
+  `periode` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `nota`
+--
+
+INSERT INTO `nota` (`id_nota`, `id_barang`, `id_member`, `jumlah`, `total`, `tanggal_input`, `periode`) VALUES
+(1, 'BR001', 1, '4', '12000', '6 October 2020, 0:45', '10-2020'),
+(2, 'BR001', 1, '4', '12000', '6 October 2020, 0:45', '10-2020'),
+(3, 'BR001', 1, '4', '12000', '6 October 2020, 0:45', '10-2020'),
+(4, 'BR001', 1, '4', '12000', '6 October 2020, 0:45', '10-2020'),
+(5, 'BR001', 1, '2', '6000', '6 October 2020, 0:49', '10-2020'),
+(6, 'BR001', 1, '2', '6000', '6 October 2020, 0:49', '10-2020'),
+(7, 'BR001', 1, '2', '6000', '6 October 2020, 1:15', '10-2020'),
+(8, 'BR002', 1, '2', '6000', '6 October 2020, 1:17', '10-2020'),
+(9, 'BR001', 1, '2', '6000', '6 October 2020, 1:20', '10-2020'),
+(10, 'BR001', 1, '2', '6000', '6 October 2020, 1:51', '07-2021'),
+(11, 'BR003', 1, '1', '2000', '14 July 2021, 18:13', '07-2021'),
+(12, 'BR003', 1, '1', '2000', '14 July 2021, 23:42', '07-2021'),
+(13, 'BR004', 1, '1', '12000', '15 July 2021, 22:07', '07-2021'),
+(14, 'BR004', 1, '2', '24000', '15 July 2021, 22:07', '07-2021'),
+(15, 'BR012', 1, '1', '400000', '16 July 2021, 17:54', '07-2021'),
+(16, 'BR011', 1, '1', '650001', '16 July 2021, 17:54', '07-2021'),
+(17, 'BR011', 1, '1', '650001', '16 July 2021, 18:17', '07-2021'),
+(18, 'BR010', 1, '1', '5000000', '16 July 2021, 18:17', '07-2021'),
+(19, 'BR012', 1, '2', '800000', '17 July 2011, 6:46', '07-2011'),
+(20, 'BR010', 1, '1', '5000000', '17 July 2011, 6:46', '07-2011'),
+(21, 'BR012', 1, '1', '400000', '20 November 2021, 21:29', '11-2021'),
+(22, 'BR012', 1, '1', '400000', '20 November 2021, 21:50', '11-2021');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `penjualan`
+--
+
+CREATE TABLE `penjualan` (
+  `id_penjualan` int(11) NOT NULL,
+  `id_barang` varchar(255) NOT NULL,
+  `id_member` int(11) NOT NULL,
+  `jumlah` varchar(255) NOT NULL,
+  `total` varchar(255) NOT NULL,
+  `tanggal_input` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `penjualan`
+--
+
+INSERT INTO `penjualan` (`id_penjualan`, `id_barang`, `id_member`, `jumlah`, `total`, `tanggal_input`) VALUES
+(38, 'BR012', 1, '1', '400000', '20 November 2021, 21:50');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `toko`
+--
+
+CREATE TABLE `toko` (
+  `id_toko` int(11) NOT NULL,
+  `nama_toko` varchar(255) NOT NULL,
+  `alamat_toko` text NOT NULL,
+  `tlp` varchar(255) NOT NULL,
+  `nama_pemilik` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `toko`
+--
+
+INSERT INTO `toko` (`id_toko`, `nama_toko`, `alamat_toko`, `tlp`, `nama_pemilik`) VALUES
+(1, 'DUA MASA', '', '', '');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `barang`
+--
+ALTER TABLE `barang`
+  ADD PRIMARY KEY (`kode_barang`);
+
+--
+-- Indexes for table `kategori`
+--
+ALTER TABLE `kategori`
+  ADD PRIMARY KEY (`id_kategori`);
+
+--
+-- Indexes for table `login`
+--
+ALTER TABLE `login`
+  ADD PRIMARY KEY (`id_login`);
+
+--
+-- Indexes for table `member`
+--
+ALTER TABLE `member`
+  ADD PRIMARY KEY (`id_member`);
+
+--
+-- Indexes for table `nota`
+--
+ALTER TABLE `nota`
+  ADD PRIMARY KEY (`id_nota`);
+
+--
+-- Indexes for table `penjualan`
+--
+ALTER TABLE `penjualan`
+  ADD PRIMARY KEY (`id_penjualan`);
+
+--
+-- Indexes for table `toko`
+--
+ALTER TABLE `toko`
+  ADD PRIMARY KEY (`id_toko`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `barang`
+--
+ALTER TABLE `barang`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+
+--
+-- AUTO_INCREMENT for table `kategori`
+--
+ALTER TABLE `kategori`
+  MODIFY `id_kategori` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+--
+-- AUTO_INCREMENT for table `login`
+--
+ALTER TABLE `login`
+  MODIFY `id_login` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `member`
+--
+ALTER TABLE `member`
+  MODIFY `id_member` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `nota`
+--
+ALTER TABLE `nota`
+  MODIFY `id_nota` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
+
+--
+-- AUTO_INCREMENT for table `penjualan`
+--
+ALTER TABLE `penjualan`
+  MODIFY `id_penjualan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=40;
+
+--
+-- AUTO_INCREMENT for table `toko`
+--
+ALTER TABLE `toko`
+  MODIFY `id_toko` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
